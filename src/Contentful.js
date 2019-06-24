a565f1d75aadd9dbd5b387a1aef664559f5defd4
OctaveInfinity <e.makoviak@gmail.com>
2019-06-24 14:47:49 +0300

    Update Contentful.js

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,8 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient ({
-    space: "k4a248fsxjrm",
-    // space: process.env.REACT_APP_API_SPACE,
-    accessToken: "xhtQZsoH60377UAaAWMtnesPqHteoj5JJaX6xxK9VEY",
-    // accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
